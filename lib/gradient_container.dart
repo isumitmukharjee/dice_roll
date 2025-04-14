34f0b6266aab9bc57a0ab6528908c09403af0300
Sumit Mukharjee <sumit1mukharjee@gmail.com>
2025-04-14 12:37:02 +0530

    flutter

---
+++ lib/gradient_container.dart
@@ -0,0 +1,53 @@
+import 'package:first_dice_app/text_class.dart';
+import 'package:flutter/material.dart';
+
+
+//variables
+
+const startAlignment = Alignment.topLeft; //never be reassignedgit
+const endAlignment = Alignment.bottomRight;
+// dart is also an object oriented language
+class GradientContainer extends StatelessWidget {
+  const GradientContainer( this.color1,  this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+  // final List<Color> colors;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: TextContainer("Hello World"),
+      ),
+    );
+  }
+}
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration:  BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors;
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: Center(
+//         child: TextContainer("Hello World"),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/text_class.dart
@@ -0,0 +1,18 @@
+// ignore: depend_on_referenced_packages
+import 'package:flutter/material.dart';
+
+class TextContainer extends StatelessWidget {
+   const TextContainer(this.outputText, {super.key});
+
+  final String outputText;
+
+  @override
+  Widget build(context) {
+    return Text(
+      outputText,
+      style: TextStyle(
+        color: Colors.white, 
+      fontSize: 28),
+    );
+  }
+}
